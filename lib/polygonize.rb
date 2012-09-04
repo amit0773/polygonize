67eb05a463e7f2299490ec5f8416526bae119e79
milligramme <milligramme.cc@gmail.com>
2012-09-04 19:29:37 +0900

    add coordinates method

--- lib/polygonize.rb
+++ lib/polygonize.rb
@@ -1,5 +1,27 @@
 require "polygonize/version"
 
 module Polygonize
-  # Your code goes here...
+  def coordinates(txt)
+    ar1 = []
+    ar2 = []
+    ret = {}
+
+    txt.split(//).each do | char |
+      u = format("%x", char.unpack("U*")[0])
+      ar1 << u.scan(/../)
+    end
+
+    # 奇数の場合は "00" を追加して、偶数配列にする
+    ar1.flatten!
+    ar1 << "00" if ar1.flatten.size%2 == 1
+  
+    ar1.each_slice(2) do | a,b |
+      ar2 << [a, b].map! { |c| c.hex }.join(",")
+    end
+
+    ret['points'] = ar2.join(" ")
+    ret['text'] = txt
+    ret
+  end  
+  
 end
--- spec/polygonize_spec.rb
+++ spec/polygonize_spec.rb
@@ -1,2 +1,45 @@
+# encoding: UTF-8
+
 require 'spec_helper'
 
+include Polygonize
+
+describe "Polygonize" do
+  context "空テキスト" do
+    it "空テキストはreturn[point]に何も返さない" do
+      coordinates("")['points'].should eql ""
+    end
+    
+    it "空テキストはreturn[text]に何も返さない" do
+      coordinates("")['text'].should eql ""
+    end
+    
+  end
+  
+  context "テキスト" do
+    txt = "こんにちは"
+    it "return[text] は元の文章を返す" do
+      coordinates(txt)['text'].should eql txt
+    end
+  
+    it "要素は偶数個である" do
+      ar = coordinates(txt)['points'].split(" ").inject([]) { |mem, x| mem << x.split(",") }
+      (ar.flatten.size%2).should eql 0
+    end
+    
+    it "各要素は255(FF)以下である" do
+      coordinates(txt)['points'].split(" ").flatten.reject! { |x| x.to_i < 255}
+    end
+  end
+
+  context "1バイトテキスト" do
+    txt_e = "x"
+    it "ゼロで補完する" do
+      coordinates(txt_e)['points'].split(",")[1].should eql "0"
+    end
+    
+    it "x = 0x78" do
+      coordinates(txt_e)['points'].split(",")[0].should eql "78".hex.to_s
+    end
+  end
+end
